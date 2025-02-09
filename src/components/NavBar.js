1c0d0a9fc9845425b2b0a0e75b25d66208212302
Souka001 <soukainaiziki2001@gmail.com>
2025-02-09 17:13:23 +0100

    Last Modifications

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,102 +1,112 @@
 import { useState, useEffect } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
-import navicon1 from '../assets/img/navicon1.svg';
-import navicon3 from '../assets/img/navicon3.svg';
-import iconFooter from '../assets/img/iconFooter.svg';
+import logo from "../assets/img/logo.svg";
+import navicon1 from "../assets/img/navicon1.svg";
+import navicon3 from "../assets/img/navicon3.svg";
+import iconFooter from "../assets/img/iconFooter.svg";
 
 import { HashLink as Link } from "react-router-hash-link"; // ✅ Use HashLink
 
 function NavBar() {
-    const [activeLink, setActiveLink] = useState('home');
-    const [scrolled, setScrolled] = useState(false);
+  const [activeLink, setActiveLink] = useState("home");
+  const [scrolled, setScrolled] = useState(false);
 
-    useEffect(() => {
-        const onScroll = () => {
-            if (window.scrollY > 70) {
-                setScrolled(true);
-            } else {
-                setScrolled(false);
-            }
-        };
-
-        window.addEventListener("scroll", onScroll);
-        return () => window.removeEventListener("scroll", onScroll);
-    }, []);
-    
-    const onUpdateActiveLink = (value) => {
-        setActiveLink(value);
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 70) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
     };
 
-    return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
-            <Container>
-                {/* Logo link */}
-                <Navbar.Brand as={Link} to="/#"> {/* ✅ Redirect home */}
-                    <img className="logonavbar" src={logo} alt="Logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav">
-                    <span className="navbar-toggler-icon"></span>
-                </Navbar.Toggle>
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
 
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        {/* Home */}
-                        <Nav.Link 
-    as={Link} 
-    to="/#home" 
-    className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} 
-    onClick={() => onUpdateActiveLink('home')}
->
-    Home
-</Nav.Link>
+  const onUpdateActiveLink = (value) => {
+    setActiveLink(value);
+  };
 
-<Nav.Link 
-    as={Link} 
-    to="/#skills" 
-    className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} 
-    onClick={() => onUpdateActiveLink('skills')}
->
-    Skills
-</Nav.Link>
+  return (
+    <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+      <Container>
+        {/* Logo link */}
+        <Navbar.Brand as={Link} to="/#">
+          {" "}
+          {/* ✅ Redirect home */}
+          <img className="logonavbar" src={logo} alt="Logo" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav">
+          <span className="navbar-toggler-icon"></span>
+        </Navbar.Toggle>
 
-<Nav.Link 
-    as={Link} 
-    to="/#projects" 
-    className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} 
-    onClick={() => onUpdateActiveLink('projects')}
->
-    Projects
-</Nav.Link>
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {/* Home */}
+            <Nav.Link
+              as={Link}
+              to="/#home"
+              className={
+                activeLink === "home" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("home")}
+            >
+              Home
+            </Nav.Link>
 
-<Nav.Link 
-    as={Link} 
-    to="/#contact" 
-    className={activeLink === 'contact' ? 'active navbar-link' : 'navbar-link'} 
-    onClick={() => onUpdateActiveLink('contact')}
->
-    Contact
-</Nav.Link>
-                    </Nav>
+            <Nav.Link
+              as={Link}
+              to="/#skills"
+              className={
+                activeLink === "skills" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("skills")}
+            >
+              Skills
+            </Nav.Link>
 
-                    {/* Social Media Icons */}
-                    <span className="navbar-text">
-                        <div className="social-icon">
-                            <a href="https://www.linkedin.com/in/soukaina-azaqui-931924185/">
-                                <img src={navicon1} alt="icon_linkedin" />
-                            </a>
-                            <a href="https://www.instagram.com/az__souka/profilecard/?igsh=MXNiNTZqZjRubHY3">
-                                <img src={navicon3} alt="icon_instagram" />
-                            </a>
-                            <a href="mailto:soukainaiziki2001@gmail.com">
-                                <img src={iconFooter} alt="icon_gmail"/>
-                            </a>
-                        </div>
-                    </span>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+            <Nav.Link
+              as={Link}
+              to="/#projects"
+              className={
+                activeLink === "projects" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("projects")}
+            >
+              Projects
+            </Nav.Link>
+
+            <Nav.Link
+              as={Link}
+              to="/#contact"
+              className={
+                activeLink === "contact" ? "active navbar-link" : "navbar-link"
+              }
+              onClick={() => onUpdateActiveLink("contact")}
+            >
+              Contact
+            </Nav.Link>
+          </Nav>
+
+          {/* Social Media Icons */}
+          <span className="navbar-text">
+            <div className="social-icon">
+              <a href="https://www.linkedin.com/in/soukaina-azaqui-931924185/">
+                <img src={navicon1} alt="icon_linkedin" />
+              </a>
+              <a href="https://www.instagram.com/az__souka/profilecard/?igsh=MXNiNTZqZjRubHY3">
+                <img src={navicon3} alt="icon_instagram" />
+              </a>
+              <a href="mailto:soukainaiziki2001@gmail.com">
+                <img src={iconFooter} alt="icon_gmail" />
+              </a>
+            </div>
+          </span>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavBar;
