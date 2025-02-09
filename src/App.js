0a9dc3e613486e3cb66bc1b825a27de7af7883c6
Souka001 <soukainaiziki2001@gmail.com>
2025-02-09 01:11:12 +0100

    Fixed routing issues by switching to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
